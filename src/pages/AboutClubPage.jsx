9a29917d31919596c48c63cbf0258f850e40ec01
anikisana <younker12345@gmail.com>
2023-01-27 15:30:58 +0600

    Сделали компоненту Rules

--- src/pages/AboutClubPage.jsx
+++ src/pages/AboutClubPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import {Header, PhotoKart,AboutClub} from '../components/About'
+import {Header, PhotoKart, AboutClub, Rules} from '../components/About'
 
 import './aboutPage.scss';
 
@@ -12,6 +12,7 @@ const AboutClubPage = () => {
         <div className="contaier">
           <PhotoKart />
           <AboutClub />
+          <Rules />
         </div>
         <Link to="/">Main</Link>
     </>
