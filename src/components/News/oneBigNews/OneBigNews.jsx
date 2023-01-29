73bea84265b78b6d872ac907c7a43741215e42cb
anikisana <younker12345@gmail.com>
2023-01-29 18:30:28 +0600

    Сделана стилизация BigNews

--- src/components/News/oneBigNews/OneBigNews.jsx
+++ src/components/News/oneBigNews/OneBigNews.jsx
@@ -4,9 +4,9 @@ import s from './oneBigNews.module.scss';
 
 const OneBigNews = ({title, img, titleInfo, time}) => {
   return (
-    <div className={s.news_item}>
+    <div className={s.news_item + " " + s.big}>
         <div className={s.img}>
-          <img src={img} alt={title} />
+          <img src={img} alt={title} className={s.imgBig}/>
         </div>
         <p className={s.time}>{time}</p>
         <div className={s.title}>{title}</div>
